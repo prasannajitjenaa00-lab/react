fbb2bff815d0238a860e4106f0b532e198a81995
PRASANNAJIT JENA <prasannajitjenaa00@gmail.com>
2026-06-08 19:58:31 +0530

    Describe your changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <> 
-  <h1>hello</h1>
-    <h2>prasannajit jena</h2>
-    </>
+function apple (){
+  return(
+    <h1>hello </h1>
   )
-}
 
-export default App
+}
+export default apple
